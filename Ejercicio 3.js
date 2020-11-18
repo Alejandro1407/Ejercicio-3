a9b56874fee14f99592099662e58e875b6c58a42
Alejandro1407 <alejandroalejo714@gmail.com>
2020-11-18 10:54:17 -0600

    CRUD completado

--- Ejercicio 3.js
+++ Ejercicio 3.js
@@ -14,17 +14,27 @@ document.getElementById("Form").addEventListener("submit", function (event) {
         'FechaFundacion': document.getElementById('txtFecha').value,
         'Direccion':document.getElementById('txtDireccion').value
     }
-    datos.push(formData);
+
+    let id = document.getElementById("txtID").value
+
+    if(id != ""){
+        formData.Id = id
+        datos.splice(id-1,1,formData)
+        console.log(formData)
+    }else{
+        datos.push(formData);
+    }    
     sessionStorage.setItem("Datos",JSON.stringify(datos))
     limpiar()
     obtenerDatos()
 })
 
 function limpiar(){
-    document.getElementById("txtNombre").innerHTML = "";
-    document.getElementById("txtNIT").innerHTML = "";
-    document.getElementById("txtFecha").innerHTML = "";
-    document.getElementById("txtDireccion").innerHTML = "";
+    document.getElementById("txtID").value = "";
+    document.getElementById("txtNombre").value = "";
+    document.getElementById("txtNIT").value = "";
+    document.getElementById("txtFecha").value = "";
+    document.getElementById("txtDireccion").value = "";
 }
 
 function obtenerDatos(){
@@ -56,19 +66,36 @@ function obtenerDatos(){
             var textoCelda5 = document.createTextNode(empresa.Direccion);
             celda5.appendChild(textoCelda5);
             container.appendChild(celda5);
+
             var button = document.createElement("button")
             button.textContent = "Eliminar"
             button.id = empresa.Id
-           
             button.onclick = function(event){
                 eliminarDato(event.target.id);
-
+            }
+            var deletebutton = document.createElement("button")
+            deletebutton.textContent = "Editar"
+            deletebutton.id = empresa.Id
+            deletebutton.onclick = function(event){
+                editarDatos(empresa);
             }
             container.appendChild(button)
+            container.appendChild(deletebutton)
             tableBody.appendChild(container);
+
         }
 
     )}
+
+function editarDatos(empresa){
+
+    document.getElementById("txtID").value = empresa.Id
+    document.getElementById("txtNombre").value = empresa.Nombre
+    document.getElementById("txtNIT").value = empresa.NIT
+    document.getElementById("txtFecha").value = empresa.FechaFundacion
+    document.getElementById("txtDireccion").value = empresa.Direccion
+}
+
 
 
 function eliminarDato(Id){
